faa51c43c41b322a312a558f73c0dc9d7c4f0b28
Nicolas Allende <nicolas1671@yahoo.com.ar>
2025-05-05 19:33:46 -0300

    Actualizando ramas

--- frontend/src/components/NavBar/NavBar_Event_Details/NaBar_Event_Details.tsx
+++ frontend/src/components/NavBar/NavBar_Event_Details/NaBar_Event_Details.tsx
@@ -1,16 +1,14 @@
-import NavBar from '../NavBar';
 import Link from "next/link";
 import Image from "next/image";
 
 export const NavBar_Event_Details = () => {
     return (
-        <div className="w-full h-full flex flex-col justify-end">
+        <div className="w-full h-full flex flex-col justify-end mt-6">
             <div className="flex flex-col gap-6">
                 <div className="flex flex-col items-center">
-                    <Link href="/dashboard/overview"><Image src="/add_circle.svg" alt="Logo" width={60} height={60}/></Link>
+                    <Link href="/Add_Spent"><Image src="/add_circle.svg" alt="Logo" width={60} height={60}/></Link>
                     <p className="text-[#FAFF00]">Agregar gasto</p>
                 </div>            
-                <NavBar />
             </div>
         </div>
     );
