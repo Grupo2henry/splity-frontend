5d4e3bc09a75ea0273c48a947d2d1b6a4e92ad78
Gonzalo Murua <gonzaloemurua96@gmail.com>
2025-05-06 17:40:28 -0300

    Cambios en Failure

--- frontend/src/app/Failure/page.tsx
+++
@@ -1,7 +0,0 @@
-import FailureComponent from "@/app/Failure/Failure"; // Ajusta la ruta si es necesario
-
-const FailurePage = () => {
-  return <FailureComponent />;
-};
-
-export default FailurePage;
