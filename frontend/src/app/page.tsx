671da73a797f8157bad592303b106d0a40595c62
Gonzalo Murua <gonzaloemurua96@gmail.com>
2025-05-21 16:53:53 -0300

    Cambios para despligue

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import Image from "next/image";
+//import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
-import { motion } from "framer-motion";
+//import { motion } from "framer-motion";
 
 export default function Home() {
   return (
