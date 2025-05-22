d828ddc54a7884c17f9b64b009ead2ad57e4fac1
Gonzalo Murua <gonzaloemurua96@gmail.com>
2025-05-22 00:18:32 -0300

    Minimos cambios en checkout button

--- frontend/src/components/CheckoutButton/CheckoutButton.tsx
+++ frontend/src/components/CheckoutButton/CheckoutButton.tsx
@@ -2,15 +2,15 @@
 
 import { createMercadoPagoPreferenceAndRedirect } from "@/services/mercadopago-services/mercadopago-services";
 import styles from "./CheckoutButton.module.css";
-import { useRouter } from "next/navigation";
+
 
 
 export default function CheckoutButton() {
-  const router = useRouter();
+
 
   const handleCheckout = async () => {
     await createMercadoPagoPreferenceAndRedirect();
-    router.push("/subscription");
+
   };
 
   return (
