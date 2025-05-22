4fb8d30c414d5d45375de0b7db9512eee0f1b025
Chesars <cesarponce19544@gmail.com>
2025-05-21 21:28:19 -0300

    update: button

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -62,14 +62,16 @@ export default function NavBar() {
           </Link>
           <nav className="flex items-center">
             <ul className={styles.navList}>
-              <li className="flex items-center">
-                <Link 
-                  href="/subscription" 
-                  className={`${styles.navLink} bg-transparent text-white px-3 py-1 rounded-lg font-semibold transition-all duration-300 flex items-center justify-center gap-2 border border-[#7FFFD4] shadow-[0_0_10px_rgba(127,255,212,0.3)] hover:shadow-[0_0_15px_rgba(127,255,212,0.5)] hover:border-[#A0FFE6] text-xs transform-none hover:transform-none`}
-                >
-                  Suscribirse
-                </Link>
-              </li>
+              {user && (
+                <li className="flex items-center">
+                  <Link 
+                    href="/subscription" 
+                    className={`${styles.navLink} bg-transparent text-white px-3 py-1 rounded-lg font-semibold transition-all duration-300 flex items-center justify-center gap-2 border border-[#7FFFD4] shadow-[0_0_10px_rgba(127,255,212,0.3)] hover:shadow-[0_0_15px_rgba(127,255,212,0.5)] hover:border-[#A0FFE6] text-xs transform-none hover:transform-none`}
+                  >
+                    Suscribirse
+                  </Link>
+                </li>
+              )}
               <li>
                 <Link href="/" className={styles.navLink}>
                   Home
