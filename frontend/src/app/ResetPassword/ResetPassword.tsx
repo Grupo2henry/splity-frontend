5a31db3a3a8b0c2f70a44271768c4b65d33eaeea
Nicolas Allende <nicolas1671@yahoo.com.ar>
2025-05-21 23:01:27 -0300

    Reset y forgot terminados

---
+++ frontend/src/app/ResetPassword/ResetPassword.tsx
@@ -0,0 +1,12 @@
+import ResetPasswordForm from "@/components/Forms/ForgotPasswordForm/ResetPasswordForm";
+
+export const ResetPassword = () => {
+    return (
+        <div>
+            <p>Ingresa tu nueva contraseña</p>
+            <ResetPasswordForm />
+        </div>
+    );
+};
+
+export default ResetPassword;
