d5ebedf7a0735b7a148ceb312e75f12151edd192
Gonzalo Murua <gonzaloemurua96@gmail.com>
2025-05-21 23:45:02 -0300

    Arreglos de Google Map

--- frontend/src/app/Update_Event/[slug]/Update_Event.tsx
+++ frontend/src/app/Update_Event/[slug]/Update_Event.tsx
@@ -3,18 +3,25 @@
 
 import { useParams } from "next/navigation";
 import EventForm from "@/components/Forms/EventForm/EventForm";
+import { useMembership } from "@/context/MembershipContext";
 
 const UpdateEventPage = () => {
   const { slug } = useParams();
+  const { actualGroupMembership } = useMembership();
 
-  // Asegurarse de que slug sea siempre un string antes de pasarlo
   const eventSlug = typeof slug === 'string' ? slug : undefined;
+
+  if (!eventSlug) {
+    return <div>Error: ID del evento no encontrado.</div>;
+  }
+
+  if (!actualGroupMembership) {
+    return <div className="text-white">Cargando evento...</div>; // o un spinner
+  }
 
   return (
     <div className="p-4">
-      {/* <h2 className="text-xl font-bold mb-4 text-white">Actualizar Evento</h2> */}
-      {eventSlug && <EventForm slug={eventSlug} />}
-      {!eventSlug && <div>Error: ID del evento no encontrado.</div>}
+      <EventForm slug={eventSlug} />
     </div>
   );
 };
